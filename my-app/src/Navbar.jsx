81a05905301a1515113ad165b32c1393ab2cb430
vinothdurai-j <vinothdurai137@gmail.com>
2025-03-14 10:42:38 +0530

    Update Navbar.jsx

--- my-app/src/Navbar.jsx
+++ my-app/src/Navbar.jsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { Link, useLocation } from "react-router-dom";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min'; // Ensure Bootstrap JS is imported
 
 const Navbar = () => {
   const location = useLocation();
+
+  // Navbar items
   const navItems = [
     { name: "Home", path: "/" },
     { name: "Available Books", path: "/available-books" },
@@ -13,8 +16,13 @@ const Navbar = () => {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light mb-4">
-      <div className="container">
-        <Link className="navbar-brand" to="/">Book Store</Link>
+      <div className="container-fluid">
+        {/* Brand/Logo */}
+        <Link className="navbar-brand" to="/">
+          Book Store
+        </Link>
+
+        {/* Hamburger Button for Mobile */}
         <button
           className="navbar-toggler"
           type="button"
@@ -26,8 +34,10 @@ const Navbar = () => {
         >
           <span className="navbar-toggler-icon"></span>
         </button>
+
+        {/* Collapsible Navbar Content */}
         <div className="collapse navbar-collapse" id="navbarNav">
-          <ul className="navbar-nav">
+          <ul className="navbar-nav ms-auto"> {/* ms-auto aligns items to the right */}
             {navItems.map(({ name, path }, index) => (
               <li className="nav-item" key={index}>
                 <Link
